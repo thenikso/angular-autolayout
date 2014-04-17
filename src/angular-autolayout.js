4c7b41b88463db52f3a81772145e5b4c9c0e83cf
Nicola Peduzzi <thenikso@gmail.com>
2014-04-17 18:05:55 +0200

    Add attributeConverters and relations to autolayout provider.

--- src/angular-autolayout.js
+++ src/angular-autolayout.js
@@ -9,6 +9,123 @@
 	// Main `autolayout` service
 	angular.module('autolayout').provider('autolayout', function() {
 
+		var provider = this;
+
+		provider.relations = {
+			equal: function(a, b, priority) {
+				return new c.Equation(a, b, c.Strength.medium, priority || 0)
+			},
+			greaterOrEqual: function(a, b, priority) {
+				return new c.Inequality(a, c.GEQ, b, c.Strength.medium, priority || 0);
+			},
+			lessOrEqual: function(a, b, priority) {
+				return new c.Inequality(a, c.LEQ, b, c.Strength.medium, priority || 0);
+			}
+		};
+
+		provider.attributeConverters = {
+			top: {
+				create: function(el) {
+					var ctx = function(solver) {
+						return ctx.top;
+					};
+					ctx.top = new c.Variable({
+						value: el[0].offsetTop
+					});
+					return ctx;
+				},
+				materialize: function(el, ctx) {
+					console.log(el, 'top', ctx.top.value + 'px');
+					el.css('top', ctx.top.value + 'px');
+				}
+			},
+			left: {
+				create: function(el) {
+					var ctx = function(solver) {
+						return ctx.left;
+					};
+					ctx.left = new c.Variable({
+						value: el[0].offsetLeft
+					});
+					return ctx;
+				},
+				materialize: function(el, ctx) {
+					console.log(el, 'left', ctx.left.value + 'px');
+					el.css('left', ctx.left.value + 'px');
+				}
+			},
+			width: {
+				create: function(el) {
+					var initialWidth = el[0].offsetWidth;
+					var ctx = function(solver) {
+						solver.addConstraint(new c.Inequality(ctx.width, c.GEQ, 0, c.Strength.required));
+						solver.addConstraint(new c.Inequality(ctx.width, c.GEQ, initialWidth, c.Strength.weak));
+						return ctx.width;
+					};
+					ctx.width = new c.Variable({
+						value: initialWidth
+					});
+					return ctx;
+				},
+				materialize: function(el, ctx) {
+					console.log(el, 'width', ctx.width.value + 'px');
+					el.css('width', ctx.width.value + 'px');
+				}
+			},
+			height: {
+				create: function(el) {
+					var initialHeight = el[0].offsetHeight;
+					var ctx = function(solver) {
+						solver.addConstraint(new c.Inequality(ctx.height, c.GEQ, 0, c.Strength.required));
+						solver.addConstraint(new c.Inequality(ctx.height, c.GEQ, initialHeight, c.Strength.weak));
+						return ctx.height;
+					};
+					ctx.height = new c.Variable({
+						value: initialHeight
+					});
+					return ctx;
+				},
+				materialize: function(el, ctx) {
+					console.log(el, 'height', ctx.height.value + 'px');
+					el.css('height', ctx.height.value + 'px');
+				}
+			},
+			right: {
+				create: function(el) {
+					var leftCtx = Autolayout.attribute.left(el);
+					var widthCtx = Autolayout.attribute.width(el);
+					var ctx = function(solver) {
+						leftCtx(solver);
+						widthCtx(solver);
+						return c.plus(ctx.left, ctx.width);
+					}
+					angular.extend(ctx, leftCtx, widthCtx);
+					return ctx
+				},
+				materialize: function(el, ctx) {
+					Autolayout.attribute.left(el, ctx);
+					Autolayout.attribute.width(el, ctx);
+				}
+			},
+			bottom: {
+				create: function(el) {
+					var leftCtx = Autolayout.attribute.top(el);
+					var widthCtx = Autolayout.attribute.height(el);
+					var ctx = function(solver) {
+						leftCtx(solver);
+						widthCtx(solver);
+						return c.plus(ctx.top, ctx.height);
+					}
+					angular.extend(ctx, leftCtx, widthCtx);
+					return ctx
+				},
+				materialize: function(el, ctx) {
+					Autolayout.attribute.top(el, ctx);
+					Autolayout.attribute.height(el, ctx);
+				},
+			}
+		};
+
 		function Autolayout(container) {
 			if (!(this instanceof Autolayout)) {
 				return new Autolayout(container);
@@ -23,7 +140,7 @@
 			}
 		};
 
-		this.$get = ['$rootElement', '$rootScope',
+		provider.$get = ['$rootElement', '$rootScope',
 			function($rootElement, $rootScope) {
 				Autolayout.$rootElement = $rootElement;
 				Autolayout.$rootScope = $rootScope;
--- test/autolayoutProvider-spec.js
+++ test/autolayoutProvider-spec.js
@@ -22,4 +22,21 @@ describe('Angular Autolayout Provider', function() {
 		expect(autolayoutProvider).to.not.be.undefined;
 	});
 
+	it('should define default relations', function() {
+		expect(autolayoutProvider.relations).to.not.be.undefined;
+		expect(autolayoutProvider.relations.equal).to.be.a("function");
+		expect(autolayoutProvider.relations.greaterOrEqual).to.be.a("function");
+		expect(autolayoutProvider.relations.lessOrEqual).to.be.a("function");
+	});
+
+	it('should define default attribute converters', function() {
+		expect(autolayoutProvider.attributeConverters).to.not.be.undefined;
+		expect(autolayoutProvider.attributeConverters.left).to.not.be.undefined;
+		expect(autolayoutProvider.attributeConverters.top).to.not.be.undefined;
+		expect(autolayoutProvider.attributeConverters.width).to.not.be.undefined;
+		expect(autolayoutProvider.attributeConverters.height).to.not.be.undefined;
+		expect(autolayoutProvider.attributeConverters.right).to.not.be.undefined;
+		expect(autolayoutProvider.attributeConverters.bottom).to.not.be.undefined;
+	});
+
 });
