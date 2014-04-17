6a605897dbc5928cf99d6d8a77187172d1d151d5
Nicola Peduzzi <thenikso@gmail.com>
2014-04-17 15:15:52 +0200

    Add cassowary to karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function(config) {
 
 		// list of files / patterns to load in the browser
 		files: [
+			'node_modules/cassowary/bin/c.js',
 			'bower_components/angular/angular.js',
 			'bower_components/angular-mocks/angular-mocks.js',
 			'bower_components/chai/chai.js',
